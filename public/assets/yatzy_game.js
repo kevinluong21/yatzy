0e98bae479aec3c293472793c851118aaacc96ea
Jessica Chen <Jessica10chen@gmail.com>
2024-06-10 14:44:24 -0400

    Update yatzy_game.js

--- public/assets/yatzy_game.js
+++ public/assets/yatzy_game.js
@@ -1,25 +1,13 @@
-// In `yatzy_game.js`, implement a current state of a game.
-// A yatzy game comprises of a turn, which includes
-
-// * Which roll you are on (0, 1, 2, or 3)
-// * Current value on each of the 5 dice
-// * Keep / re-roll state of each dice
-
-// The `YatzyGame` should focus on tracking the state of the game
-// without knowing much about the rules, that comes next!
-
 import Dice from './dice.js';
 
-class YatzyGame {
-    constructor(){
-        this.roll = 0;
-        this.diceValues = [0, 0, 0, 0, 0];
-        this.diceKeepState = [false, false, false, false, false];
-        this.dice = Dice.create(6);
-    }
+var yatzyGame = (function() {
+    var roll = 0;
+    var diceValues = [0, 0, 0, 0, 0];
+    var diceKeepState = [false, false, false, false, false];
+    var dice = Dice.create(6);
 
     // Roll the dice, depending on the roll and keep/re-roll state of the dice
-    rollDice() { 
+    function rollDice() { 
         if(this.roll == 3){ // When on maximum roll
             throw new Error("On maximum number of rolls for turn");
         }
@@ -33,19 +21,21 @@ class YatzyGame {
     }
 
     // Keep state of the dice
-    keepDice(i) {
+    function keepDice(i) {
         this.diceKeepState[i] = true;
     }
 
     // Reroll state of the dice
-    rerollDice(i) { 
+    function rerollDice(i) { 
         this.diceKeepState[i] = false;
     }
 
     // Reset the game
-    resetGame(){ 
+    function resetGame(){ 
         this.roll = 0;
         this.diceValues = [0, 0, 0, 0, 0];
         this.diceKeepState = [false, false, false, false, false];
     }
-}
+
+    return yatzyGame;
+} ());
