86aeacc7119bb89eb328227cfc11b78c967f3a36
Jessica Chen <Jessica10chen@gmail.com>
2024-06-10 15:13:05 -0400

    Fix errors

--- public/assets/yatzy_game.js
+++ public/assets/yatzy_game.js
@@ -1,41 +1,46 @@
 import Dice from './dice.js';
 
-var yatzyGame = (function() {
-    var roll = 0;
-    var diceValues = [0, 0, 0, 0, 0];
-    var diceKeepState = [false, false, false, false, false];
-    var dice = Dice.create(6);
+var YatzyGame = (function() {
+    var game = {}
+    game.roll = 0;
+    game.diceValues = [0, 0, 0, 0, 0];
+    game.diceKeepState = [false, false, false, false, false];
+
+    game.dice = new Dice();
+    game.dice.create(6);
 
     // Roll the dice, depending on the roll and keep/re-roll state of the dice
-    function rollDice() { 
-        if(this.roll == 3){ // When on maximum roll
+    game.rollDice = function(){ 
+        if(game.roll == 3){ // When on maximum roll
             throw new Error("On maximum number of rolls for turn");
         }
 
-        for(var i=0;i<this.diceValues.length;i++){
-            if(!this.diceKeepState[i]){
-                this.diceValues[i] = dice.roll();
+        for(var i=0;i<game.diceValues.length;i++){
+            if(!game.diceKeepState[i]){
+                game.diceValues[i] = game.dice.roll() + 1;
             }
         }
-        this.roll++;
+        game.roll++;
     }
 
     // Keep state of the dice
-    function keepDice(i) {
-        this.diceKeepState[i] = true;
+    game.keepDice = function(i){
+        game.diceKeepState[i] = true;
     }
 
     // Reroll state of the dice
-    function rerollDice(i) { 
-        this.diceKeepState[i] = false;
+    game.rerollDice = function(i){
+        game.diceKeepState[i] = false;
     }
 
     // Reset the game
-    function resetGame(){ 
-        this.roll = 0;
-        this.diceValues = [0, 0, 0, 0, 0];
-        this.diceKeepState = [false, false, false, false, false];
+    game.resetGame = function(){
+        game.roll = 0;
+        game.diceValues = [0, 0, 0, 0, 0];
+        game.diceKeepState = [false, false, false, false, false];
     }
 
-    return yatzyGame;
+    return game;
 } ());
+
+export default YatzyGame;
