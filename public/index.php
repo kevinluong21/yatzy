7f144ca960e1cc06e2ec41319057dba07f959e22
Kevin Luong <kluon030@uottawa.ca>
2024-07-19 21:27:55 -0400

    version.onclick ajax call

--- public/index.php
+++ public/index.php
@@ -39,7 +39,18 @@ $_SESSION["numGames"] = 1;
         const output = document.getElementById("output");
         const version = document.getElementById("version");
         version.onclick = function(e) {
-        output.innerHTML = "Look up version clicked";
+            const xmlhttp = new XMLHttpRequest();
+
+            xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp.status == 200) {
+                output.innerHTML = xmlhttp.responseText;
+                }
+            }
+            };
+
+            xmlhttp.open("GET", "/api.php", true);
+            xmlhttp.send();
         }
         </script>
         <div class="round">
